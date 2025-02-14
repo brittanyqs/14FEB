5cd47252f8a4cb1aba6e0eef1c70ff2abf148dbf
brittanyqs <165699245+brittanyqs@users.noreply.github.com>
2025-02-14 13:20:44 -0600

    Delete 14FEB directory

--- 14FEB/script.js
+++
@@ -1,9 +0,0 @@
-function animateCard(card) {
-    // Añadimos una clase temporal para animación al hacer clic
-    card.classList.add('clicked');
-    
-    // Después de un pequeño delay, eliminamos la clase para restaurar el estado original
-    setTimeout(function() {
-        card.classList.remove('clicked');
-    }, 200); // El tiempo en milisegundos que dura la animación de clic
-}
